9bae9e2bceb8bf8dd8fa97dc4a0a1575da086b02
Ling-Hong <33500893+Ling-Hong@users.noreply.github.com>
2018-06-27 18:26:39 +0800

    Create Operating_sys.py

---
+++ Operating_sys.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+'''
+reference:
+https://stackoverflow.com/questions/26545051/is-there-a-way-to-delete-created-variables-functions-etc-from-the-memory-of-th
+'''
+
+
+# get current directory
+
+# change directory
+
+# delete all objects in the environment
+
+# view all objects 
+dir()
+
